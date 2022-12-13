f4afb55c3d32669b384f82d85a173d60001b46a3
JangYeonWoo <dusdn0516@naver.com>
2022-12-13 16:29:34 +0900

    JS:Navbar menu click

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar__dark');
+  } else {
+    navbar.classList.remove('navbar__dark');
+  }
+});
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+});
